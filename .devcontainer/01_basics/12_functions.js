223b2ab8557fcb6847efe820a386611b9a914c9b
vaibhaw491 <vaibhaw491@gmail.com>
2025-02-06 16:46:41 +0530

    add function

---
+++ .devcontainer/01_basics/12_functions.js
@@ -0,0 +1,57 @@
+function sayMyName() {
+    console.log("v");
+    console.log("a");
+    console.log("i");
+    console.log("b");
+    console.log("h");
+    console.log("a");
+    console.log("w");
+
+}
+// sayMyName()
+
+// function addTwoNumbers(number1, number2) {
+//     console.log(number1 + number2);
+// }
+// addTwoNumbers(2, 3)
+// addTwoNumbers(2, "3")
+// addTwoNumbers(2, null)
+// const result = addTwoNumbers(2, 3)
+
+// console.log("Result: ", result);
+
+function addTwoNumbers(number1, number2) {
+    // let result = number1 + number2
+    // return result
+    return number1 + number2
+}
+const result = addTwoNumbers(2, 3)
+
+// console.log("Result: ", result);
+
+function loginUserMessage(username) {
+    return `${username} just logged in`
+}
+// console.log(loginUserMessage("vaibhaw")) 
+// console.log(loginUserMessage())
+
+function loginUserMessage(username) {
+
+
+    if (username === undefined) {
+        console.log("please enter a username")
+        return
+
+    }
+    return `${username} just logged in`
+}
+// console.log(loginUserMessage("vaibhaw")) 
+console.log(loginUserMessage())
+
+
+
+
+
+
+
+
