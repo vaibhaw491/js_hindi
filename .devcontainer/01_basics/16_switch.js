eaf39a7fef9ff15a8f773f34622c5af4e4c7c4e7
vaibhaw491 <vaibhaw491@gmail.com>
2025-02-10 11:14:16 +0530

    switch

---
+++ .devcontainer/01_basics/16_switch.js
@@ -0,0 +1,58 @@
+// switch (key) {
+//     case value:
+
+//     break;
+
+//     default:
+//         break;
+// }
+
+// const month = 3
+
+// switch (month) {
+
+//     case 1:
+//         console.log("Janruary");
+//         break;
+
+//     case 2:
+//         console.log("February");
+//         break;
+
+//     case 3:
+//         console.log("March");
+//         break;
+
+//     case 4:
+//         console.log("April");
+//         break;
+
+//     default:
+//         console.log("default case match");
+
+// }
+
+const month = "march"
+
+switch (month) {
+
+    case "jan":
+        console.log("Janruary");
+        break;
+
+    case "feb":
+        console.log("February");
+        break;
+
+    case "march":
+        console.log("March");
+        break;
+
+    case "apr":
+        console.log("April");
+        break;
+
+    default:
+        console.log("default case match");
+
+}
